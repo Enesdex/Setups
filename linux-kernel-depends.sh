890fa6ffa6821f3a55ee4b9cf7a8bc0c9fa8d977
Selim Enes Karaduman <51437998+Enesdex@users.noreply.github.com>
2021-10-17 15:41:42 +0300

    Create linux-kernel-depends.sh

---
+++ linux-kernel-depends.sh
@@ -0,0 +1,8 @@
+# /bin/bash
+apt install qemu-system-x86 
+
+apt-get install gcc make g++ 
+
+apt-get install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf git
+
+
