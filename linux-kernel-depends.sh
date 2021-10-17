3e4a9cd2aeeec07e39d2cd33437e9b46842d6eaa
Selim Enes Karaduman <51437998+Enesdex@users.noreply.github.com>
2021-10-17 15:42:34 +0300

    Update linux-kernel-depends.sh

--- linux-kernel-depends.sh
+++ linux-kernel-depends.sh
@@ -1,8 +1,8 @@
 # /bin/bash
-apt install qemu-system-x86 
+apt install qemu-system-x86 -y
 
-apt-get install gcc make g++ 
+apt-get install gcc make g++ -y
 
-apt-get install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf git
+apt-get install libncurses-dev gawk flex bison openssl libssl-dev dkms libelf-dev libudev-dev libpci-dev libiberty-dev autoconf git -y
 
 
